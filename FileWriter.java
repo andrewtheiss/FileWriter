83c88827d460fc4d07d2734cd9d6b84a245774e6
Andrew Theiss <andrew.theiss@gmail.com>
2023-08-25 10:23:16 -0700

    Updated filewriter

--- FileWriter.java
+++ FileWriter.java
@@ -2,4 +2,9 @@ public class FileWriter {
     public void write(String message) {
         System.out.println("Writing to file: " + message);
     }
+
+    public String read(String fileName) {
+        System.out.println("Reading from file: " + fileName);
+        return "Hello World";
+    }
 }
